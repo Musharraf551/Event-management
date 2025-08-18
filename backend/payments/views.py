f2ecc9b9d39e903d74e5f95b9603e41189be7310
musharraf <musharrafaleem2@gmail.com>
2025-08-18 14:38:59 +0530

    created CreateBookingView in views.py of payments app

--- backend/payments/views.py
+++ backend/payments/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Booking
+from .serializers import BookingSerializer
 
-# Create your views here.
+class CreateBookingView(generics.CreateAPIView):
+    serializer_class = BookingSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+# Later we will add Stripe/Razorpay payment integration here
