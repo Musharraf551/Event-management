e23729c734003159677840a89fdae26d8aedfd2b
musharraf <musharrafaleem2@gmail.com>
2025-08-18 14:36:21 +0530

    created Boking model in payments app

--- backend/payments/models.py
+++ backend/payments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from users.models import CustomUser
+from events.models import Event
 
-# Create your models here.
+class Booking(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    paid = models.BooleanField(default=False)
+    payment_id = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.event.title}"
