d3933abd7fcc8d6a54d5f8b6155247d14f09613d
musharraf <musharrafaleem2@gmail.com>
2025-08-18 14:29:23 +0530

    created Event model in events app

--- backend/events/models.py
+++ backend/events/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='events/')
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    created_by = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
