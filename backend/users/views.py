f112fe8ce48cda51f10202b18b79f2a9d9634f27
musharraf <musharrafaleem2@gmail.com>
2025-08-18 14:20:10 +0530

    created RegisterView and ProfileView in views.py of users app

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import CustomUser
+from .serializers import UserRegisterSerializer, UserSerializer, UserTokenSerializer
+from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 
-# Create your views here.
+# Register user
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserRegisterSerializer
+
+# Profile view
+class ProfileView(generics.RetrieveUpdateAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        return self.request.user
+
